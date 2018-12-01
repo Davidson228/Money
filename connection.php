0dda4c5a4305e18301bbc619c980a722a9007075
Davidson228 <davidarinsh@gmail.com>
2018-12-01 09:24:19 +0200

    test

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 session_start();
 
 $db = new
-\atk4\data\Persistence_SQL('mysql:dbname=heroku_33a09646a43f60a;host=eu-cdbr-west-02.cleardb.net','root','');
+\atk4\data\Persistence_SQL('mysql:dbname=heroku_33a09646a43f60a;host=eu-cdbr-west-02.cleardb.net','b40ba71796d5af','a0bf7181');
 class ReminderBox extends \atk4\ui\View {
   public $ui = 'piled segment';
   public function setModel(\atk4\data\Model $guests) {
